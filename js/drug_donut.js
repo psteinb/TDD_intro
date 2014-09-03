0d7dafecb63985e9e27436868e05c8fd4807c45e
Peter Steinbach <steinbac@mpi-cbg.de>
2014-09-03 22:16:22 +0200

    finished intro

---
+++ js/drug_donut.js
@@ -0,0 +1,17 @@
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Category', 'Landmark Publications'],
+        ['Reproducible',     6],
+        ['Irreproducible',      47]
+    ]);
+
+    var options = {
+        title: 'Cancer Drug Development Pre-Clinical Studies',
+        pieHole: 0.4,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+    chart.draw(data, options);
+}
